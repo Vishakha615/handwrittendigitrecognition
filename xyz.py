3e4698d03886030095dad05a9b93d1ee0dd61be5
Vishakha615 <nikamvishakha954@gmail.com>
2026-07-16 20:36:13 +0530

    Add files via upload

---
+++ xyz.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+import cv2 
+import streamlit_drawable_canvas as sdc
+
+print("streamlit==",st.__version__)
+print("numpy==",np.__version__)
+print("tensorflow==",tf.__version__)
+print("opencv-python==",cv2.__version__)
+print("streamlit_drawable_canvas==",sdc.__version__)
